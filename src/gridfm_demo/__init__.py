eaba0cbf0505c7e3bff2afc5a2f67bfb405160f3
Étienne Bourbeau <27770178+bourdeet@users.noreply.github.com>
2026-06-29 11:45:09 +0200

    Initial commit

---
+++ src/gridfm_demo/__init__.py
@@ -0,0 +1,1 @@
+"""Demo case bencharking gridFM-derived power flow solutions with state-of-the-art deterministic solutions from pandapower."""
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""Test suite for the gridfm_demo package."""
+
+
+def test_dummy() -> None:
+    """Dummy test function, to ensure that pytest works."""
+    assert True
