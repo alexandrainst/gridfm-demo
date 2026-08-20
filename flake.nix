b47657ac7e30aa4d93061aead6a7508a9ef17301
Michael Iversen <michael.iversen@alexandra.dk>
2026-08-20 13:31:38 +0200

    add nix flake
    The nix flake contains all dependencies used for developing this
    project. The developer may drop into a nix shell with those dependencies
    installed using `nix develop`

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Nix flake for a development shell with Prettier and markdownlint-cli2";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: {
+    devShells = builtins.mapAttrs (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.prettier
+          pkgs.markdownlint-cli2
+          pkgs.uv
+          pkgs.git
+          pkgs.tree
+        ];
+      };
+    }) inputs.nixpkgs.legacyPackages;
+  };
+}
